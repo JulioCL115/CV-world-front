98e01806bb80f4eb628301520f2c840e80701105
Florencia Soldavini <florenciasoldavinivarela@gmail.com>
2024-02-08 18:21:56 -0300

    cambios en topbar, footer y curriculums

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,6 +1,6 @@
-import styles from './TopBar.module.css';
+import styles from "./TopBar.module.css";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 import Logo from "../assets/Logo-Black.png";
 
@@ -8,9 +8,9 @@ function TopBar() {
     return (
         <div className={styles.topbar}>
             <div className={styles.containerLeft}>
-                <Link className={styles.txt} to="/">Home</Link>
-                <Link className={styles.txt} to="/curriculums">Currículums</Link>
-                <Link className={styles.txt} to="/suscripciones">Suscripciones</Link>
+                <Link className={styles.txtRegular16} to="/">Home</Link>
+                <Link className={styles.txtRegular16} to="/curriculums">Currículums</Link>
+                <Link className={styles.txtRegular16} to="/suscripciones">Suscripciones</Link>
             </div>
             <div className={styles.containerCenter}>
                 <h1 className={styles.txt} id={styles.h1}>CV World</h1>
@@ -23,7 +23,7 @@ function TopBar() {
                     </svg>
                 </button>
                 <div className={styles.containerLogin}>
-                    <Link className={styles.txt} id={styles.txtUnderlined} to="/signin">Iniciar sesión</Link>
+                    <Link className={styles.txtRegular16Underlined}  to="/signin">Iniciar sesión</Link>
 
                     <svg className={styles.icn} id={styles.icnUser}xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.2" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
