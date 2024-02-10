0c2d8cc1b0df63789aa359014e18baf655650eb6
Jose Sanchez <devjosesanchezl@gmail.com>
2024-02-10 16:41:11 -0300

    solucion de coflictos

---
+++ src/components/Curriculums/Card.jsx
@@ -0,0 +1,11 @@
+import styles from "./Card.module.css"
+
+function Card () {
+    return (
+        <div className={styles.card}>
+            CARD
+        </div>
+    )
+};
+
+export default Card;
---
+++ src/components/Curriculums/Cards.jsx
@@ -0,0 +1,11 @@
+import styles from "./Cards.module.css"
+
+function Cards () {
+    return (
+        <div className={styles.cards}>
+            CARDS
+        </div>
+    )
+};
+
+export default Cards;
