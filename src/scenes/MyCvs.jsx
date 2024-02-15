775799f2f9a79ffe78d849be5ffb83f51b9469e7
Florencia Soldavini <florenciasoldavinivarela@gmail.com>
2024-02-15 15:11:10 -0300

    cambios en my cvs

--- src/scenes/MyCvs.jsx
+++ src/scenes/MyCvs.jsx
@@ -1,14 +1,31 @@
 import styles from "./MyCvs.module.css"
+import Illustration from "../assets/CV-Example-Creation-Illustration.webp"
+
 
 import { Link } from "react-router-dom";
 
 function MyCvs() {
-    
+    const storedUser = localStorage.getItem('currentUser');
+
     return (
         <div>
-            <Link to="/createcv">
-                <button >Agregar CV</button>
-            </Link>
+            <div className={styles.myCvs}>
+                <div className={styles.containerTop}>
+                    <h1 className={styles.txtSemiBold32Black}>Mis CV's</h1>
+                </div>
+                {storedUser.cvs ?
+                    <div>
+
+                    </div> :
+                    <div className={styles.containerBottom}>
+                        <img className={styles.img} src={Illustration} alt="Illustration" />
+                        <p className={styles.txtSemiBold16Purple}>Parece que todavía no tenés currículums cargados</p>
+                        <Link to="/createcv">
+                            <button className={styles.btn}>Cargar CV</button>
+                        </Link>
+                    </div>
+                }
+            </div>
         </div>
     )
 };
