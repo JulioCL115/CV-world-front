eaca1dde489b818d8ae5e9a1410f15657f49b545
Matias <SaliskyMatias@gmail.com>
2024-02-22 15:46:25 -0300

    a

--- src/scenes/main/Checkout.jsx
+++ src/scenes/main/Checkout.jsx
@@ -36,8 +36,9 @@ const Checkout = () => {
                 const paymentInfo = {
                     title: subscription.name,
                     description: "Descripción de prueba",
-                    quantity:  1,
+                    quantity: 1,
                     unit_price: subscription.price,
+                    subscriptionId: subscriptionId
                 };
 
                 try {
