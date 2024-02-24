aee2a59c48bd062c6832c4aa17946c21ce2314fd
Florencia <florenciasoldavinivarela@gmail.com>
2024-02-24 00:28:18 -0300

    landing page lista

--- src/scenes/main/Home.jsx
+++ src/scenes/main/Home.jsx
@@ -1,20 +1,32 @@
 import styles from './Home.module.css';
 
+import CvImage from "../../assets/cv_munich-1040x1433.png"
+
+import { Link } from "react-router-dom";
+
 function Home() {
+    const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+
     return (
-        <div>
+        <div className={styles.home}>
             <div className={styles.containerLeft}>
-                <h1>Causá una buena primera impresión</h1>
-                <p>Desbloquá todo tu potencial profesional,
-                    con CV World no sólo creás tu CV, creás una excelente primera impresión
-                </p>
-                <div>
-                    <button>Crear CV</button>
-                    <button>Explorar CV's</button>
+                <div className={styles.containerTxt}>
+                    <h1 className={styles.txtSemiBold48Black}>Causá una buena primera impresión</h1>
+                    <p className={styles.txtRegular16Black}>Desbloquá todo tu potencial profesional,
+                        con CV World no sólo creás tu CV, creás una excelente primera impresión
+                    </p>
+                    <div className={styles.containerBtn}>
+                        <Link to={currentUser ? "/mycvs" : "/signin"} >
+                            <button className={styles.btnCreate}>Crear CV</button>
+                        </Link>
+                        <Link to="/curriculums">
+                            <button className={styles.btnExplore}>Explorar CV's</button>
+                        </Link>
+                    </div>
                 </div>
             </div>
             <div className={styles.containerRight}>
-
+                <img className={styles.img} src={CvImage} alt="CvImage" />
             </div>
         </div>
     )
