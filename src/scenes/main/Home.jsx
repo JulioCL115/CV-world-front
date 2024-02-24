f3dc57010af0e510a4f1fd29b5fb1d2796987bd9
Florencia <florenciasoldavinivarela@gmail.com>
2024-02-23 21:01:07 -0300

    empece a crear la landing page

--- src/scenes/main/Home.jsx
+++ src/scenes/main/Home.jsx
@@ -1,9 +1,21 @@
-
+import styles from './Home.module.css';
 
 function Home() {
     return (
         <div>
-            Home
+            <div className={styles.containerLeft}>
+                <h1>Causá una buena primera impresión</h1>
+                <p>Desbloquá todo tu potencial profesional,
+                    con CV World no sólo creás tu CV, creás una excelente primera impresión
+                </p>
+                <div>
+                    <button>Crear CV</button>
+                    <button>Explorar CV's</button>
+                </div>
+            </div>
+            <div className={styles.containerRight}>
+
+            </div>
         </div>
     )
 };
