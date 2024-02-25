594837c0bd08e4b24a3bfc1bdd9e7c1a7edbfc07
Florencia <florenciasoldavinivarela@gmail.com>
2024-02-25 15:57:21 -0300

    cambios para que funcione bien delete cv

--- src/redux/actions/cvs/deleteCv.js
+++ src/redux/actions/cvs/deleteCv.js
@@ -1,15 +1,15 @@
 import axiosInstance from "../../../config/axios-config";
 import axios from "axios";
 
-const deleteCv = async(cvId) => {
+const deleteCv = async (cvId) => {
     const endpoint = axios.defaults.baseURL + "cv/delete/" + cvId;
 
     try {
         const response = await axiosInstance.put(endpoint)
 
-        console.log("Response:", response.data);
+        return true
     } catch (error) {
-        console.log("Error:", error);
+        return false
     }
 
 };
