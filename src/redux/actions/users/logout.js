651f31ed92dcbd0a5e64777711542954471a69c9
Matias <SaliskyMatias@gmail.com>
2024-02-25 14:08:14 -0300

    update

--- src/redux/actions/users/logout.js
+++ src/redux/actions/users/logout.js
@@ -3,6 +3,7 @@ const logout = async () => {
         try {    
             localStorage.removeItem('token');
             localStorage.removeItem('currentUser');
+            localStorage.removeItem('authToken');
             window.dispatchEvent(new Event('storage'));
         } catch (error) {
             console.log(error);
