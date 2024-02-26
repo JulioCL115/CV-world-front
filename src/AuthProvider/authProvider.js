0a6fd9743047585c6e3d2486668d2b4e70c08ef5
Florencia <florenciasoldavinivarela@gmail.com>
2024-02-26 16:46:31 -0300

    cambios post category

--- src/AuthProvider/authProvider.js
+++ src/AuthProvider/authProvider.js
@@ -3,18 +3,22 @@ import React, { createContext, useState, useEffect } from 'react';
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
- const [token, setToken] = useState(() => {
-    const token = localStorage.getItem('token');
-    return token;
- });
+   const [token, setToken] = useState(() => {
+      const token = localStorage.getItem('token');
+      return token;
+   });
 
- useEffect(() => {
-    localStorage.setItem('token', JSON.stringify(token));
- }, [token]);
+   useEffect(() => {
+      if (token !== null && token !== undefined) {
+         localStorage.setItem('token', JSON.stringify(token));
+      } else {
+         localStorage.removeItem('token');
+      }
+   }, [token]);
 
- return (
-    <AuthContext.Provider value={{ token, setToken }}>
-      {children}
-    </AuthContext.Provider>
- );
+   return (
+      <AuthContext.Provider value={{ token, setToken }}>
+         {children}
+      </AuthContext.Provider>
+   );
 };
